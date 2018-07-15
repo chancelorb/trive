18201f99eff2acc1bce307a2e1255e8ddb3fa5d8
Chance Baars <chance@Chance.home>
2018-07-14 23:22:25 -0400

    setup p2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navbar from "./components/navbar";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src="https://trive.news/wp-content/uploads/2018/03/trive-logo-icon.png" className="App-logo" alt="logo" />
+          <br /><br /><br />
+          <img src="https://trive.news/wp-content/uploads/2018/01/discover-banner-4.png" alt="description" />
+          <br /><br />
         </header>
         <p className="App-intro">
           Ik ben chance
---
+++ src/components/navbar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styles = {
+  root: {
+    flexgrow: 1,
+  },
+  flex: {
+    flexgrow: 1,
+  },
+  menuButton: {
+    marginLeft: "auto",
+    marginRight: 20,
+  },
+};
+
+function ButtonAppBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position='static' color="inherit">
+        <Toolbar>
+
+          <Typography variant="title" color="inherit" className={classes.flex}>
+            News
+          </Typography>
+          {/* <Button color="inherit">Login</Button> */}
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonAppBar);
